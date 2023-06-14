871f0214193dc9d94fd72c95d26a72968fb897c5
Robert <ishbuldinrobert2002@gmail.com>
2023-06-14 13:30:05 +0500

    hw9 done

---
+++ ex66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: 
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNaturalElements(int min, int max)
+{
+    if (max == min) return min;
+    else return max + SumNaturalElements(min, max - 1);
+}
+
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumNaturalElements(min, max));
